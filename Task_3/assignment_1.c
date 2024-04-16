b1d61a48b58e3b1048680e8b53a9acf878594ce5
ahmedgyar <ahmedyassergyar@gmail.com>
2024-04-16 13:01:41 +0200

    2024-04-16 01:01:41 PM

---
+++ Task_3/assignment_1.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+
+float add(float num1, float num2) {
+    return num1 + num2;
+}
+
+float subtract(float num1, float num2) {
+    return num1 - num2;
+}
+
+float multiply(float num1, float num2) {
+    return num1 * num2;
+}
+
+float divide(float num1, float num2) {
+    if (num2 != 0) {
+        return num1 / num2;
+    } else {
+        printf("Error: Division by zero!\n");
+        return 0;
+    }
+}
+
+int main() {
+    float num1, num2;
+    int operation;
+    
+printf("choose the operation\n\
+1-additon\n2-subtration\n3-multiblication\n4-divion\n");
+
+scanf("%d",&operation);
+    
+
+    switch (operation) {
+        case 1:
+            printf("enter the first number:");
+            scanf("%f",&num1);
+            printf("enter the second number:");
+            scanf("%f",&num2);
+            printf("the result is %f", add(num1,num2));
+            break;
+        case 2:
+            printf("enter the first number:");
+            scanf("%f",&num1);
+            printf("enter the second number:");
+            scanf("%f",&num2);
+            printf("the result is %f", subtract(num1,num2));
+            break;
+        case 3:
+             printf("enter the first number:");
+            scanf("%f",&num1);
+            printf("enter the second number:");
+            scanf("%f",&num2);
+           printf("the result is %f", multiply(num1,num2));
+            break;
+        case 4:
+             printf("enter the first number:");
+            scanf("%f",&num1);
+            printf("enter the second number:");
+            scanf("%f",&num2);
+           printf("the result is %f", divide(num1,num2));
+            break;
+        default:
+            break;
+    }
+
+    return 0;
+}
